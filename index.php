a2caf2bda7e3b1f17a4c64ed211bbc8351e182fc
ishaaqilham <63960531+ishaaqilham@users.noreply.github.com>
2020-05-15 20:55:02 +0530

    login form created

---
+++ index.php
@@ -0,0 +1,8 @@
+<form name="login">
+  <label> Username </label>
+  <input type="text" placeholder="Enter username">
+  <label> Password </label>
+  <input type="password" placeholder="Enter username">
+  <br>
+  <input type="submit" value="Login">
+</form>
